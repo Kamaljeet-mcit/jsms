a24b38f8dca58c051da47ee9ce69c0207dc141f8
Kamaljeet-mcit <83106098+Kamaljeet-mcit@users.noreply.github.com>
2022-03-18 19:34:46 +0530

    Add files via upload

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,1 +1,25 @@
+<?php
 
+
+define('DB_SERVER','bpfhuevzdtbtp2akhpzi-mysql.services.clever-cloud.com');
+
+define('DB_USER','uiwjqlctrc1xxudl');
+
+define('DB_PASS' ,'uqi8WE2CCJMJOcj80wTX');
+
+define('DB_NAME', 'bpfhuevzdtbtp2akhpzi');
+
+
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+// Check connection
+
+if (mysqli_connect_errno())
+
+{
+
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+}
+
+?>
