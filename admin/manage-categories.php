3578dd40144967c1dca081f5e961a340bf757db4
Kamaljeet-mcit <83106098+Kamaljeet-mcit@users.noreply.github.com>
2022-03-19 08:33:41 +0530

    Update manage-categories.php

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -32,7 +32,7 @@ echo "<script>window.location.href='manage-categories.php'</script>";
                     <div class="container-fluid px-4">
                         <h1 class="mt-4">Manage Categories</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
                             <li class="breadcrumb-item active">Manage Categories</li>
                         </ol>
                         <div class="card mb-4">
